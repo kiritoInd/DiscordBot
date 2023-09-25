aa7740d52aa04928f7652b8014bfb54e17dda7ba
kiritoInd <shreyansh69420@gmail.com>
2023-09-26 02:02:39 +0530

    v1

--- auto.py
+++ auto.py
@@ -1,14 +1,17 @@
 import discord
 import random
 
-token = "YOUR BOT TOKEN"  
-channel_id = "channel id"
+token = "YOUR TOKEN"  
+channel_id = "Channel ID"
 
 messages = ["chacha gandu h", "chacha chokke", "suiii", "jadli wah sai hato" , "anmol bkl" , "torimakiechode" ,"chutmarika"]
+messages2 = ["accha card chaiye" , "card lelo" , "ew phir agye"]
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
+
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
@@ -21,8 +24,11 @@ async def on_message(message):
     print(f"Received message: {message.content}")
     if message.author == client.user:
         return
-    if message.content == '':
+    if message.content == 'Roast Deadman':
         response = random.choice(messages)
+        await message.channel.send(response)
+    if message.content == '<@285291001055019008> is dropping the cards':
+        response = random.choice(messages1)
         await message.channel.send(response)
 
 client.run(token)
